e5147cd185d5d50e286945b96c3b86133b5b00f0
franklantz <71349553+frank-o-lantz@users.noreply.github.com>
2022-12-27 13:36:43 -0800

    added functionality and fixed bugs

--- dice.js
+++ dice.js
@@ -1,13 +1,22 @@
 // get the grid container element
-const gridContainer = document.getElementById("grid-container");
+var gridContainer = document.getElementById("grid-container");
 
 // create an array of max values for each dice
-var diceMaxValues = Array.from({length: 100}, () => Math.floor(Math.random() * 99) + 2);
+var diceMaxValues = Array.from({length: 25}, () => Math.floor(Math.random() * 99) + 2);
 
-for (let i = 0; i < 10; i++) {
-  for (let j = 0; j < 10; j++) {
+// initialize click counter
+var clickCounter = 100;
+
+// set a game over flag
+var gameoverFlag = 0;
+
+
+// generate the dice grid
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; j < 5; j++) {
     // use the max value for the current dice from the diceMaxValues array
-    let dice = Math.floor(Math.random() * diceMaxValues[i]) + 1;
+    let pointer = (i*5);
+    let dice = Math.floor(Math.random() * (diceMaxValues[pointer+j] - 1)) + 1;
     let diceElement = document.createElement("div");
     diceElement.className = "dice";
     diceElement.textContent = dice;
@@ -17,67 +26,136 @@ for (let i = 0; i < 10; i++) {
   gridContainer.appendChild(lineBreak);
 }
 
+// calculate initial score
 window.addEventListener('load', function() {
   // re-calculate the score
-  let updatedScore = calcScore();
+  var updatedScore = calcScore();
   // update the text content of the score element
-  scoreElement.textContent = "Score: " + updatedScore;
+  scoreElement.textContent = labelElement.textContent + updatedScore;
 });
-
 
 
 // get all dice elements
 const diceElements = document.querySelectorAll('.dice');
 
 
-
 // add a click event listener to each dice element
 diceElements.forEach((dice, i) => {
   dice.addEventListener('click', function() {
-    // use the max value for the current dice from the diceMaxValues array
-    let newDiceValue = Math.floor(Math.random() * diceMaxValues[i]) + 1;
+  
+  if (clickCounter > 0) {
+  clickCounter--;
+  counterElement.textContent = 'Clicks Remaining: ' + clickCounter;
+
+    var newDiceValue = Math.floor(Math.random() * diceMaxValues[i]) + 1;
     dice.textContent = newDiceValue;
     dice.classList.add('blink');
     dice.addEventListener('animationend', function() {
       dice.classList.remove('blink');
-      let updatedScore = calcScore();
-      scoreElement.textContent = "Score: " + updatedScore;
-    });
+      updatedScore = calcScore();
+      scoreElement.textContent = labelElement.textContent + updatedScore;
+      });
+  
+  }  
+
+  if (clickCounter < 1) {
+    gameoverFlag = 1;
+    labelElement.textContent = 'Final Score: ';
+    scoreElement.textContent = labelElement.textContent + updatedScore;
+   
+    // Remove the event listener to disable further clicks
+    dice.removeEventListener('click', arguments.callee);
+  }
+
   });
 });
 
-// create a new button element
+
+//RE-ROLL BUTTON
 const rerollButton = document.createElement('button');
-// set the text content of the button
-rerollButton.textContent = 'RE-ROLL ALL';
-// add the button to the DOM
+rerollButton.textContent = 'RE-ROLL ALL DICE';
 gridContainer.appendChild(rerollButton);
-
-rerollButton.addEventListener('click', function() {
-
-
-  diceElements.forEach((dice, i) => {
-    // use the max value for the current dice from the diceMaxValues array
-    let newDiceValue = Math.floor(Math.random() * diceMaxValues[i]) + 1;
-    dice.textContent = newDiceValue;
-
-  });
-
-    let updatedScore = calcScore();
-    scoreElement.textContent = "Score: " + updatedScore;
-
-
-});
-
 rerollButton.classList.add('reroll-button');
 
+rerollButton.addEventListener('click', function() {
+    
+  if (clickCounter > 0) { 
+  clickCounter--;
+  counterElement.textContent = 'Clicks Remaining: ' + clickCounter;  
 
+  diceElements.forEach((dice, i) => {
+    var newDiceValue = Math.floor(Math.random() * diceMaxValues[i]) + 1;
+    dice.textContent = newDiceValue;
+  });
+
+    updatedScore = calcScore();
+    scoreElement.textContent = labelElement.textContent + updatedScore;
+      
+  }
+    
+  if (clickCounter < 1) {
+    gameoverFlag = 1;
+    labelElement.textContent = 'Final Score: ';
+    scoreElement.textContent = labelElement.textContent + updatedScore;
+   
+  }    
+
+});
+
+
+//RE-SET BUTTON
+const resetButton = document.createElement('button');
+resetButton.textContent = 'RESET MAX VALUES AND RE-ROLL';
+gridContainer.appendChild(resetButton);
+resetButton.classList.add('reset-button'); 
+
+resetButton.addEventListener('click', function() {
+    
+if (clickCounter > 0) {
+  clickCounter--;
+  counterElement.textContent = 'Clicks Remaining: ' + clickCounter;      
+  
+var newDiceMaxValues = Array.from({length: 25}, () => Math.floor(Math.random() * 99) + 2);
+  diceMaxValues = newDiceMaxValues;
+
+  diceElements.forEach((dice, i) => {
+    var newDiceValue = Math.floor(Math.random() * diceMaxValues[i]) + 1;
+    dice.textContent = newDiceValue;
+  });
+  
+  updatedScore = calcScore();
+  scoreElement.textContent = labelElement.textContent + updatedScore;
+    
+}
+    
+  if (clickCounter < 1) {
+    gameoverFlag = 1;
+    labelElement.textContent = 'Final Score: ';
+    scoreElement.textContent = labelElement.textContent + updatedScore;
+   
+  }      
+  
+});
+
+
+//CLICK COUNTER
+// Create a new div element to display the counter
+const counterElement = document.createElement('div');
+counterElement.classList.add('counter');
+// Set the text content of the counter element
+counterElement.textContent = 'Clicks Remaining: ' + clickCounter;
+// Add the counter element to the DOM
+gridContainer.appendChild(counterElement);
+
+
+
+// SCORE
 // create a new div element to display the score
 const scoreElement = document.createElement('div');
 // add a class name to the score element
 scoreElement.classList.add('score');
 // create a new span element to display the label
-const labelElement = document.createElement('span');
+var labelElement = document.createElement('span');
 // set the text content of the label element
 labelElement.textContent = 'Score: ';
 // append the label element to the score element
@@ -92,6 +170,7 @@ scoreElement.appendChild(valueElement);
 gridContainer.appendChild(scoreElement);
 
 
+
 function calcScore() {
   let score = 0;
   diceElements.forEach(dice => {
@@ -100,27 +179,7 @@ function calcScore() {
   return score;
 }
 
-const resetButton = document.createElement('button');
-resetButton.textContent = 'RESET MAX VALUE AND RE-ROLL';
-gridContainer.appendChild(resetButton);
 
-resetButton.addEventListener('click', function() {
-  
- 
 
-const newDiceMaxValues = Array.from({length: 100}, () => Math.floor(Math.random() * 99) + 2);
-  diceMaxValues = newDiceMaxValues;
-
-  diceElements.forEach((dice, i) => {
-    // use the max value for the current dice from the diceMaxValues array
-    let newDiceValue = Math.floor(Math.random() * diceMaxValues[i]) + 1;
-    dice.textContent = newDiceValue;
-  });
-  
-  let updatedScore = calcScore();
-  scoreElement.textContent = "Score: " + updatedScore;
-  
- 
-});
 
 
